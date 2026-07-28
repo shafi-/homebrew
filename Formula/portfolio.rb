0ec4a114a1e6a644df1d58702768951fa50f3843
shafi- <shafi.cse.buet@gmail.com>
2026-07-28 07:10:56 +0600

    Fix checksum for portfolio v0.2.2

--- Formula/portfolio.rb
+++ Formula/portfolio.rb
@@ -5,10 +5,10 @@ class Portfolio < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/shafi-/portfolio/releases/download/v0.2.2/portfolio-darwin-arm64"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "d6d94b059189b88c13e6506c587ed6ad6b9ebfbd68b4258ed5af92c4f1cf38fc"
   else
     url "https://github.com/shafi-/portfolio/releases/download/v0.2.2/portfolio-darwin-amd64"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "d6d94b059189b88c13e6506c587ed6ad6b9ebfbd68b4258ed5af92c4f1cf38fc"
   end
 
   def install
